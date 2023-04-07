be03b5c9b1a920b7d37e092742a584bc70e2079e
ineska01 <weronika.kotla@gmail.com>
2023-04-07 20:18:50 +0200

    add count area

--- task_13_geoproject.py
+++ task_13_geoproject.py
@@ -1,16 +1,19 @@
 import geopandas as gpd
+import json
+from shapely.geometry import shape
 geojson_path = "sciezka_do_mojego_geojsona"
 
 class CountGeo:
     def __init__(self, path):
         self.path = path
         self.loaded_geojson = None
-    
+        self.geometry = shape(self.loaded_geojson["geometry"])
+        
     def load_geojson(self):
         self.loaded_geojson = gpd.read_file(self.path)
     
     def count_area(self):
-        pass
+        return self.geometry.area
     
     def count_circuit(self):
         pass
@@ -22,6 +25,7 @@ class CountGeo:
         return area, circuit
     
 result_counting_poligon = CountGeo(geojson_path)
+print(result_counting_poligon.count_area())
 result_counting_poligon.load_geojson()
 result_counting_poligon.loaded_geojson
 result_counting_poligon.get_data()
